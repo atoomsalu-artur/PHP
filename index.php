482970479d861edeaf31f078b56cfbedc485cc9a
atoomsalu-artur <atoomsalu@hkhk.edu.ee>
2026-03-05 12:24:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<?php include('config.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Bootstrap demo</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+
+
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<div class="container-fluid">
+
+<a class="navbar-brand" href="#">Autorent</a>
+
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+<span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+<li class="nav-item">
+<a class="nav-link active" aria-current="page" href="#">Home</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="#">Link</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link disabled" aria-disabled="true">Disabled</a>
+</li>
+
+</ul>
+
+<form class="d-flex" role="search">
+
+<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
+
+<button class="btn btn-outline-success" type="submit">
+Search
+</button>
+
+</form>
+
+</div>
+</div>
+</nav>
+
+
+
+<!-- menüü -->
+ <div class="container mt-4">
+<div class="row row-cols-1 row-cols-md-4">
+
+
+<div class="col-12 col-md-6 col-lg-3">
+<div class="card h-100">
+<img src="https://tse1.mm.bing.net/th/id/OIP.tZRWDAwW53LVy_JHwE3j7wHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" class="card-img-top" alt="BMWW">
+<div class="card-body">
+<h5 class="card-title">BMW</h5>
+<p class="card-text">Mootor: 3.0 </p>
+<a href="#" class="btn btn-primary">LINK</a>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-3">
+<div class="card h-100">
+<img src="https://www.actualidadmotor.com/wp-content/uploads/2022/08/audi-a5-sportback-s-line-118-1.jpg" class="card-img-top" alt="Audi A5">
+<div class="card-body">
+<h5 class="card-title">AUDI A5</h5>
+<p class="card-text">uus</p>
+<a href="#" class="btn btn-primary">LINK</a>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-3">
+<div class="card h-100">
+<img src="https://th.bing.com/th/id/OIP.SOh49F2RHf_o5TKHQqo_vQHaEK?w=323&h=182&c=7&r=0&o=7&pid=1.7&rm=3" class="card-img-top" alt="OPEL">
+<div class="card-body">
+<h5 class="card-title">OPEL</h5>
+<p class="card-text">MOOTOR 1.0</p>
+<a href="#" class="btn btn-primary">LINK</a>
+</div>
+</div>
+</div>
+
+<div class="col-12 col-md-6 col-lg-3">
+<div class="card h-100">
+<img src="https://static1.topspeedimages.com/wordpress/wp-content/uploads/2024/06/2024-mercedes-benz-gle-2.jpg" class="card-img-top" alt="MERCEDES">
+<div class="card-body">
+<h5 class="card-title">MERCEDES</h5>
+<p class="card-text">Mootor: 5.0</p>
+<a href="#" class="btn btn-primary">LINK</a>
+
+</div>
+</div>
+</div>
+
+</div>
+</div>
